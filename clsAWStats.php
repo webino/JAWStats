f5cd2842361b3eefb0fb63eafe369355276a07e1
Daniel Mellum <mellum@gmail.com>
2014-03-13 12:12:50 +0100

    Fix encoding issues and added missing "pages" key
    There are some issues with char encoding from the geo-ip plugins. By
    forcing utf8 it seems to be working better. There where also a missing
    label value «pages» for «OS» and «BROWSER» causing the Browser and OS
    tab not to work.
    
    Tested with: AWSTATS DATA FILE 7.2 (build 1.992) + plugins: geoip,
    geoip_city_maxmind

--- clsAWStats.php
+++ clsAWStats.php
@@ -70,7 +70,7 @@ class clsAWStats
         $this->sFileName = $sFileName;
 
         if (is_readable($sFilePath)) {
-            $this->sAWStats = file_get_contents($sFilePath);
+            $this->sAWStats = htmlspecialchars(utf8_encode(file_get_contents($sFilePath)), ENT_SUBSTITUTE);
             $this->bLoaded  = true;
         }
         else
@@ -85,13 +85,13 @@ class clsAWStats
         $this->iTotalUnique = $this->GetSummaryElement($arrData, "totalunique", 1);
 
         // populate label array
-        $this->arrLabel["BROWSER"]                   = array("id", "hits");
+        $this->arrLabel["BROWSER"]                   = array("id", "hits", "pages");
         $this->arrLabel["DAY"]                       = array("date", "pages", "hits", "bw", "visits");
         $this->arrLabel["DOMAIN"]                    = array("id", "pages", "hits", "bw");
         $this->arrLabel["ERRORS"]                    = array("id", "hits", "bw");
         $this->arrLabel["FILETYPES"]                 = array("id", "hits", "bw", "noncompressedbw", "compressedbw");
         $this->arrLabel["KEYWORDS"]                  = array("word", "freq");
-        $this->arrLabel["OS"]                        = array("id", "hits");
+        $this->arrLabel["OS"]                        = array("id", "hits", "pages");
         $this->arrLabel["PAGEREFS"]                  = array("url", "pages", "hits");
         $this->arrLabel["ROBOT"]                     = array("id", "hits", "bw", "lastvisit", "robotstxt");
         $this->arrLabel["SEARCHWORDS"]               = array("phrase", "freq");
